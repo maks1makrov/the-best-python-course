fb9c0a4fd1412cc08a327893004fe3aca3b0ba2b
maks <maks1makrov@gmail.com>
2024-09-17 22:03:03 +0300

    drgd 8

--- src/all/views.py
+++ src/all/views.py
@@ -1,3 +1,5 @@
+import os
+
 from django.core.mail import send_mail
 from django.shortcuts import render, redirect
 from django.views import View
@@ -7,6 +9,8 @@ from all.models import Enrollment
 
 import logging
 
+from src.settings import BASE_DIR
+
 logger = logging.getLogger(__name__)
 
 
@@ -14,9 +18,16 @@ logger = logging.getLogger(__name__)
 class AllMain(View):
 
     def get(self, request):
-        context = {'form': EnrollmentForm()}
-        # return render(request, 'Copy_of_Max.html', context)
-        return render(request, 'index_g5.html', context)
+        try:
+            context = {'form': EnrollmentForm()}
+            # int('af')
+            # return render(request, 'Copy_of_Max.html', context)
+            logger.error(f"os.path.join(BASE_DIR, 'templates_not_used') - {os.path.join(BASE_DIR, 'templates_not_used')}")
+            print(os.path.join(BASE_DIR, 'templates') )
+            return render(request, 'index_vital.html', context)
+        except Exception as ex:
+            logger.error(f'{ex.__str__()}')
+            return redirect('enroll_error')
 
 
 class EnrollmentView(View):
